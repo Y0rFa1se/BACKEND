3dadee39aa5861b8891291aeaa47a84616a11e7d
Y0rFa1se <neargul123@gmail.com>
2025-02-28 04:16:01 +0900

    asdf

--- routes/database/mariadb.py
+++ routes/database/mariadb.py
@@ -9,7 +9,7 @@ router = APIRouter()
 async def api_get_stock_tickers(request: Request, password: str = None):
     host = request.client.host
     if not (host == "192.168.0.18" or host == "127.0.0.1" or host == "::1"):
-        if password != os.getenv("API_PASSWORD"):
+        if password != os.getenv("GUEST_PASSWORD"):
             return {"error": "Invalid password"}
     
     return await get_stock_tickers()
@@ -18,7 +18,7 @@ async def api_get_stock_tickers(request: Request, password: str = None):
 async def api_get_stock_prices(request: Request, ticker: str, password: str = None):
     host = request.client.host
     if not (host == "192.168.0.18" or host == "127.0.0.1" or host == "::1"):
-        if password != os.getenv("API_PASSWORD"):
+        if password != os.getenv("GUEST_PASSWORD"):
             return {"error": "Invalid password"}
     
     return await get_stock_prices(ticker)
