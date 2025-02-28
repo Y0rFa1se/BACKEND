755d9f70599ffc342ab7a41a367e54354b8fdf82
Y0rFa1se <neargul123@gmail.com>
2025-02-28 20:58:29 +0900

    asdf

--- routes/configs/test.py
+++ routes/configs/test.py
@@ -1,7 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket
 
 router = APIRouter()
 
 @router.get("/ping")
 async def ping():
     return {"response": "pong"}
+
+@router.websocket("/websocket/test")
+async def ws_test(websocket: WebSocket):
+    await websocket.accept()
+    await websocket.send_text("Hello, WebSocket!")
+
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
