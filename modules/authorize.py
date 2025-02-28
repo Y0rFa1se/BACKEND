ef0d04fa2ec7b83787fb8d3b1b454d79a2809f16
Y0rFa1se <neargul123@gmail.com>
2025-02-28 23:06:22 +0900

    asdf

--- modules/authorize.py
+++ modules/authorize.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 def authorize(host, password):
-    if host.startswith("127.0.0.1") or host.startswith("localhost") or host.startswith("192.168.1.") or host.startswith("172.16.0."):
+    if host.startswith("127.0.0.1") or host.startswith("localhost") or host.startswith("192.168.0.") or host.startswith("172.17.0.") or host.startswith("::1"):
         return True
     
     if password == os.getenv("GUEST_PASSWORD"):
